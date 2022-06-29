d2b00833ffd834ed54fdc375814e22521431caa3
vijaydev133 <vijaydeveloper004@gmail.com>
2022-06-29 08:53:27 +0530

    Modifying Code.

--- main.js
+++ main.js
@@ -47,88 +47,117 @@ var tax = document.getElementById("tax")
 var grand_total = document.getElementById("grand-total")
 
 
-
-sel_bat.onchange = function(){
-    let bat_pri = parseInt(sel_bat.value)
+function calculate(x,y,z){
     
-    let bat_unt_pri = parseInt(bat_unit.innerText)
+    let quant = parseInt(x.value)
 
-    bat_price.textContent = (bat_pri * bat_unt_pri)
+    let unit_price = parseInt(y.innerText)
+   
+    z.textContent = (quant * unit_price)
+      
+    tot.textContent = parseInt(tot.textContent) + parseInt(z.textContent)
     
-    tot.textContent = parseInt(tot.textContent) + parseInt(bat_price.textContent)
-
     tax.textContent = parseInt(tot.textContent * 0.18)
 
     grand_total.textContent = (parseInt(tot.textContent) + parseInt(tax.textContent))
+    
 
+    
 }
+
+sel_bat.onchange = function(){calculate(sel_bat,bat_unit,bat_price)}
+
+sel_ball.onchange = function(){calculate(sel_ball,ball_unit,ball_price)}
+
+sel_stump.onchange = function(){calculate(sel_stump,stump_unit,stump_price)}
+
+sel_bat_gloves.onchange = function(){calculate(sel_bat_gloves,bat_gloves_unit,bat_gloves_price)}
+
+sel_keeping_gloves.onchange = function(){calculate(sel_keeping_gloves,keeping_gloves_unit,keeping_gloves_price)}
+
+
+
+// sel_bat.onchange = function(){
+//     let bat_pri = parseInt(sel_bat.value)
+    
+//     let bat_unt_pri = parseInt(bat_unit.innerText)
+
+//     bat_price.textContent = (bat_pri * bat_unt_pri)
+    
+//     tot.textContent = parseInt(tot.textContent) + parseInt(bat_price.textContent)
+
+//     tax.textContent = parseInt(tot.textContent * 0.18)
+
+//     grand_total.textContent = (parseInt(tot.textContent) + parseInt(tax.textContent))
+
+// }
 
  
 
-sel_ball.onchange = function(){
-    let ball_pri = parseInt(sel_ball.value)
+// sel_ball.onchange = function(){
+//     let ball_pri = parseInt(sel_ball.value)
 
-    let ball_unt_pri = parseInt(ball_unit.innerText)
+//     let ball_unt_pri = parseInt(ball_unit.innerText)
 
-    ball_price.textContent = (ball_pri * ball_unt_pri)
+//     ball_price.textContent = (ball_pri * ball_unt_pri)
   
-    tot.textContent = parseInt(tot.textContent) +  parseInt(ball_price.textContent)
+//     tot.textContent = parseInt(tot.textContent) +  parseInt(ball_price.textContent)
 
-    tax.textContent = parseInt(tot.textContent * 0.18)
+//     tax.textContent = parseInt(tot.textContent * 0.18)
 
-    grand_total.textContent = (parseInt(tot.textContent) + parseInt(tax.textContent))
-}
+//     grand_total.textContent = (parseInt(tot.textContent) + parseInt(tax.textContent))
+// }
 
 
-sel_stump.onchange = function(){
-    let stump_pri = parseInt(sel_stump.value)
+// sel_stump.onchange = function(){
+//     let stump_pri = parseInt(sel_stump.value)
 
-    let stump_unt_pri = parseInt(stump_unit.innerText)
+//     let stump_unt_pri = parseInt(stump_unit.innerText)
 
-    stump_price.textContent = (stump_pri * stump_unt_pri)
+//     stump_price.textContent = (stump_pri * stump_unt_pri)
   
-    tot.textContent = parseInt(tot.textContent) +  parseInt(stump_price.textContent)
+//     tot.textContent = parseInt(tot.textContent) +  parseInt(stump_price.textContent)
     
-    tax.textContent = parseInt(tot.textContent * 0.18)
+//     tax.textContent = parseInt(tot.textContent * 0.18)
 
-    grand_total.textContent = (parseInt(tot.textContent) + parseInt(tax.textContent))
-}
-
-
+//     grand_total.textContent = (parseInt(tot.textContent) + parseInt(tax.textContent))
+// }
 
 
-sel_bat_gloves.onchange = function(){
-    let bat_gloves_pri = parseInt(sel_bat_gloves.value)
 
-    let bat_gloves_unt_pri = parseInt(bat_gloves_unit.innerText)
 
-    bat_gloves_price.textContent = (bat_gloves_pri * bat_gloves_unt_pri)
+// sel_bat_gloves.onchange = function(){
+//     let bat_gloves_pri = parseInt(sel_bat_gloves.value)
+
+//     let bat_gloves_unt_pri = parseInt(bat_gloves_unit.innerText)
+
+//     bat_gloves_price.textContent = (bat_gloves_pri * bat_gloves_unt_pri)
   
-    tot.textContent = parseInt(tot.textContent) +  parseInt(bat_gloves_price.textContent)
+//     tot.textContent = parseInt(tot.textContent) +  parseInt(bat_gloves_price.textContent)
 
     
-    tax.textContent = parseInt(tot.textContent * 0.18)
+//     tax.textContent = parseInt(tot.textContent * 0.18)
 
-    grand_total.textContent = (parseInt(tot.textContent) + parseInt(tax.textContent))
-}
-
-
+//     grand_total.textContent = (parseInt(tot.textContent) + parseInt(tax.textContent))
+// }
 
 
 
-sel_keeping_gloves.onchange = function(){
-    let keeping_gloves_pri = parseInt(sel_keeping_gloves.value)
 
-    let keeping_gloves_unt_pri = parseInt(keeping_gloves_unit.innerText)
 
-    keeping_gloves_price.textContent = (keeping_gloves_pri * keeping_gloves_unt_pri)
+// sel_keeping_gloves.onchange = function(){
+//     let keeping_gloves_pri = parseInt(sel_keeping_gloves.value)
+
+//     let keeping_gloves_unt_pri = parseInt(keeping_gloves_unit.innerText)
+
+//     keeping_gloves_price.textContent = (keeping_gloves_pri * keeping_gloves_unt_pri)
   
-    tot.textContent = parseInt(tot.textContent) +  parseInt(keeping_gloves_price.textContent)
+//     tot.textContent = parseInt(tot.textContent) +  parseInt(keeping_gloves_price.textContent)
     
-    tax.textContent = parseInt(tot.textContent * 0.18)
+//     tax.textContent = parseInt(tot.textContent * 0.18)
 
-    grand_total.textContent = (parseInt(tot.textContent) + parseInt(tax.textContent))
-}
+//     grand_total.textContent = (parseInt(tot.textContent) + parseInt(tax.textContent))
+// }
 
 
 
